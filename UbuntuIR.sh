e260168bf9770d94316fe374832212ba592bb753
adpermana <adpermana12@gmail.com>
2020-05-18 21:22:38 +0700

    Update UbuntuIR.sh

--- UbuntuIR.sh
+++ UbuntuIR.sh
@@ -20,7 +20,7 @@ cat /etc/lsb-release > $dir/2.Versi_OS.txt
 # Identifikasi Aplikasi/Service
 ps -aux > $dir/3.Daftar_Proses.txt
 top -b -n 1 > $dir/4.Daftar_Running_App.txt
-history > $dir/5.History.txt
+cat /root/.bash_history > $dir/5.History.txt
 ls /etc/cron* > $dir/6.Cron.txt
 crontab -l > $dir/7.Crontab.txt
 
